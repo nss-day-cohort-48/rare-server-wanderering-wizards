7ff907e6966f15e275a0042daed5c3d2dab2c7c5
Andrew Webb <webba@ihopu.org>
2021-07-28 09:25:33 -0500

    displayed all posts

--- posts/request.py
+++ posts/request.py
@@ -66,6 +66,61 @@ def get_posts_by_id(id):
     # Use `json` package to properly serialize list as JSON
     return json.dumps(posts)
 
+def get_all_posts():
+    with sqlite3.connect("./Rare.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+        db_cursor.execute("""
+        SELECT
+            p.id,
+            p.user_id,
+            p.category_id,
+            p.title,
+            p.publication_date,
+            p.image_url,
+            p.content,
+            p.approved,
+            c.id,
+            c.label,
+            u.id,
+            u.first_name,
+            u.last_name,
+            u.email,
+            u.bio,
+            u.username,
+            u.password,
+            u.profile_image_url,
+            u.created_on,
+            u.active
+        FROM posts p 
+        JOIN Categories c 
+        ON c.id = p.category_id
+        JOIN Users u 
+        ON u.id = p.user_id
+        """)
+
+        posts = []
+
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+
+            post = Post(row['id'], row['user_id'], row['category_id'],
+                        row['title'], row['publication_date'],
+                        row['image_url'], row['content'], row['approved'],)
+            category = Category(row['id'], row['label'])
+
+            user = User(row['id'], row['first_name'], row['last_name'],
+                        row['email'], row['bio'],
+                        row['username'], row['password'], row['profile_image_url'], row['created_on'], row['active'],)
+
+            post.category = category.__dict__
+            post.user = user.__dict__
+
+            posts.append(post.__dict__)
+
+    return json.dumps(posts)
+
 
 def get_post_details(id):
     with sqlite3.connect("./Rare.db") as conn:
